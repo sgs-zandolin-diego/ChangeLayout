46055f8bd07a3a1f22553a87edf68cc862fc791d
diego <diego@HPSRV>
2021-12-02 06:04:12 +0100

    Aggiungere i file di progetto.

---
+++ ChangeLayout.Bla/Data/ImpostazioniModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChangeLayout.Bla.Data
+{
+    public class ImpostazioniModel
+    {
+        public string AppTitle { get; set; } = "Home";
+    }
+}
