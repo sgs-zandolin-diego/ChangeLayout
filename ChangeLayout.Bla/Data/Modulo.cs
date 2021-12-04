15a31ed8c1c0c11945c89a42004f81fbada2b9fc
diego <diego@HPSRV>
2021-12-04 11:20:07 +0100

    Issue Non aggiorna il nome del brand name

---
+++ ChangeLayout.Bla/Data/Modulo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChangeLayout.Bla.Data
+{
+    public class Modulo
+    {
+        public string Nome { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
